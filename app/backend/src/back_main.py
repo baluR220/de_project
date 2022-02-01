8569e38a6bd7c8d8d29957fdcc3071e1c5d51bc4
baluR220 <1qasw23edfr45gt6@gmail.com>
2022-02-02 00:13:44 +0700

    update back_main.py

--- app/backend/src/back_main.py
+++ app/backend/src/back_main.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-from calendar import monthrange
 
-import api_scrap
+from api_scrap import APIWorker
 from common import API_URL, CITY
 
 
@@ -11,13 +10,13 @@ def get_date_interval() -> tuple:
         (year, month) = (today.year - 1, 12)
     else:
         (year, month) = (today.year, today.month - 1)
-    start_date = '%s-%s-1' % (year, month)
-    end_date = '%s-%s-31' % (year, month)
+    start_date = f'{year}-{month}-1'
+    end_date = f'{year}-{month}-31'
     return(start_date, end_date)
 
 
 if __name__ == '__main__':
     start_date, end_date = get_date_interval()
-    w = api_scrap.Worker(API_URL, CITY, start_date, end_date)
+    w = APIWorker(API_URL, CITY, start_date, end_date)
     w.scrap()
     print(w.game_id_date)
