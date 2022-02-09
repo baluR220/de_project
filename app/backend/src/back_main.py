0175f68253173e1550df65287adfde2a544540a7
baluR220 <1qasw23edfr45gt6@gmail.com>
2022-02-09 13:31:55 +0700

    add main func

--- app/backend/src/back_main.py
+++ app/backend/src/back_main.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from os import environ
 
 from api_scrap import APIWorker
-from common import API_URL, CITY
+from db_manage import DBWorker
+import common as cm
 
 
 def get_date_interval() -> tuple:
@@ -15,8 +17,14 @@ def get_date_interval() -> tuple:
     return(start_date, end_date)
 
 
+def main():
+    db_user = environ['DB_USER']
+    db_secret = environ['DB_SECRET']
+    db = DBWorker(db_user, db_secret, cm.DB_PREFIX, cm.DB_HOST, cm.DB_NAME)
+
+
 if __name__ == '__main__':
     start_date, end_date = get_date_interval()
-    w = APIWorker(API_URL, CITY, start_date, end_date)
-    w.scrap()
-    print(w.game_id_date)
+    api = APIWorker(cm.API_URL, cm.CITY, start_date, end_date)
+    api.scrap()
+    print(api.game_id_date)
