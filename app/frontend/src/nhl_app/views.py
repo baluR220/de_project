5242c40b2bd27519db06fb7983a24513af6e60bc
baluR220 <1qasw23edfr45gt6@gmail.com>
2022-02-11 21:14:51 +0700

    create views

--- app/frontend/src/nhl_app/views.py
+++ app/frontend/src/nhl_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from nhl_app.models import GameDate, GameInfo, TopTime
 
-# Create your views here.
+
+def index(request):
+    payload = {
+        'game_date': GameDate.objects.all(),
+        'game_info': GameInfo.objects.all(),
+        'top_time': TopTime.objects.all()
+    }
+    return render(request, 'nhl_app/index.html', payload)
