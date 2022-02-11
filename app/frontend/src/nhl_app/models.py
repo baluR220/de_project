2c8b844c20bf2504279bdc18b89ebedc6e2c7fc9
baluR220 <1qasw23edfr45gt6@gmail.com>
2022-02-11 21:14:37 +0700

    create models

--- app/frontend/src/nhl_app/models.py
+++ app/frontend/src/nhl_app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class GameDate(models.Model):
+    game_id = models.IntegerField(blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'game_date'
+        app_label = 'nhl_app'
+
+
+class GameInfo(models.Model):
+    game_id = models.IntegerField(blank=True, null=True)
+    away_name = models.CharField(max_length=50, blank=True, null=True)
+    away_score = models.IntegerField(blank=True, null=True)
+    home_name = models.CharField(max_length=50, blank=True, null=True)
+    home_score = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'game_info'
+        app_label = 'nhl_app'
+
+
+class TopTime(models.Model):
+    game_id = models.IntegerField(blank=True, null=True)
+    away_top_1_name = models.CharField(max_length=50, blank=True, null=True)
+    away_top_1_time = models.IntegerField(blank=True, null=True)
+    away_top_2_name = models.CharField(max_length=50, blank=True, null=True)
+    away_top_2_time = models.IntegerField(blank=True, null=True)
+    away_top_3_name = models.CharField(max_length=50, blank=True, null=True)
+    away_top_3_time = models.IntegerField(blank=True, null=True)
+    home_top_1_name = models.CharField(max_length=50, blank=True, null=True)
+    home_top_1_time = models.IntegerField(blank=True, null=True)
+    home_top_2_name = models.CharField(max_length=50, blank=True, null=True)
+    home_top_2_time = models.IntegerField(blank=True, null=True)
+    home_top_3_name = models.CharField(max_length=50, blank=True, null=True)
+    home_top_3_time = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'top_time'
+        app_label = 'nhl_app'
